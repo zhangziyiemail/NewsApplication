540fde8dea97c9e4af189cee3a24974b08604d51
zhangziyi <zhangziyiemail@gmail.com>
2020-09-21 03:03:30 -0700

    commit

--- app/src/main/java/com/example/github/newsapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/github/newsapplication/ui/home/HomeFragment.kt
@@ -3,13 +3,16 @@ package com.example.github.newsapplication.ui.home
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.github.newsapplication.R
-import com.example.github.newsapplication.base.BaseFragment
-import com.example.github.newsapplication.base.ERROR_CODE_INIT
+import com.example.github.newsapplication.base.*
 import com.example.github.newsapplication.databinding.FragmentHomeBinding
 import com.example.github.newsapplication.entity.State
 import kotlinx.android.synthetic.main.content_main.*
@@ -32,36 +35,47 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private var onInflated = true
 
     override fun actionsOnViewInflate() {
-        mViewModel.fetchCache {
-            hasCache = false
-        }
 
         fetchHomeArticleList()
-
-        mViewModel.cache?.observe(this, Observer {
-            if (onInflated){
-                onInflated = false
-                mAdapter.submitList(it)
-            }
-        })
-
-
-
-
     }
     override fun getLayoutId()= R.layout.fragment_home
 
     @SuppressLint("ClickableViewAccessibility")
     override fun initFragment(view: View, savedInstanceState: Bundle?) {
-        mBinding?.let {
-            it.refreshColor = R.color.colorAccent
+
+        mBinding?.let { binding ->
+            binding.refreshColor = R.color.colorPrimary
+            binding.refreshListener = SwipeRefreshLayout.OnRefreshListener {
+
+            }
+
+            binding.adapter = mAdapter
+
+            binding.itemLongClick = OnItemLongClickListener { position, _ ->
+
+            }
+
+            binding.articleList.setOnTouchListener { _, _ ->
+
+                false
+            }
+
+
+            binding.errorReload = ErrorReload {
+
+            }
+
+            binding.indicator = resources.getString(R.string.action_settings)
+
         }
     }
 
     fun  fetchHomeArticleList(){
+
         mViewModel.fetchHomeNews {
-            mBinding?.emptyStatus = !hasCache
+
         }
+
         mViewModel.netState?.observe(this, Observer{
             when(it.status){
                 State.RUNNING -> injectStates(true,loading= !hasCache)
@@ -69,7 +83,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                 State.FAILED ->{
                    if (it.code == ERROR_CODE_INIT){
                        injectStates(error = !hasCache)
-                       mBinding?.indicator = if(hasCache)"news" else "--"
+
                    }else{
                        requireContext().toast("no network")
                    }
@@ -78,14 +92,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         })
 
         mViewModel.articles?.observe(this, Observer {
-            mAdapter.submitList(it)
+            mAdapter.update(it)
         })
-
-
     }
 
     private fun injectStates(refreshing: Boolean = false, loading: Boolean = false, error: Boolean = false){
-        mBinding?.let {binding->
+        mBinding?.let { binding->
             binding.refreshing = refreshing
             binding.loadingStatus = loading
             binding.errorStatus = error
